2caa03a3915a19cc43b81e3fe265e40097b0e306
Ravi Dhiman <ravid7000@gmail.com>
2019-12-04 21:33:44 +0530

    Initial Commit

---
+++ ideal-doodles/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ideal-doodles
+//
+//  Created by Chahana Tyagi on 04/12/19.
+//  Copyright © 2019 Ravi Dhiman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
