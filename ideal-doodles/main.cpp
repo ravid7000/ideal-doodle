dbbee9de6406531c8fa6b48745027459de7578ed
Ravi Dhiman <ravid7000@gmail.com>
2019-12-04 21:42:02 +0530

    remove extra comment

--- ideal-doodles/main.cpp
+++ ideal-doodles/main.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  ideal-doodles
-//
-//  Created by Chahana Tyagi on 04/12/19.
-//  Copyright © 2019 Ravi Dhiman. All rights reserved.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
